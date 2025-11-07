2fecb8af6ca1baa41e6eb81e9e35591d70c8ef39
Dušan Tichý <tichy@ics.muni.cz>
2025-11-07 12:26:32 +0100

    chore(core): lower snark level

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -9,7 +9,7 @@ ssh-keygen -t ed25519 -N "" -f /config/.ssh/id_ed25519
 touch "/config/.ssh/authorized_keys"
 
 if [ -z "$USER_PUBLIC_KEY" ]; then
-  echo "User's public key not provided, what am I supposed to do?"
+  echo "User's public key not provided! No other auth methods are accepted, there's nothing to do."
   echo "Set variable $USER_PUBLIC_KEY to your public key, it will be added to authorized_keys file, so you can login with your private key."
   echo "Exiting..."
   exit 1
